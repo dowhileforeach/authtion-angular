1f93ffe0bebbf85b98da39653e49196e3589c996
nobody <nobody@gmail.com>
2018-02-15 21:09:39 +0300

    auth-button, added

---
+++ src/app/auth-button/auth-button.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-auth-button',
+  templateUrl: './auth-button.component.html',
+  styleUrls: ['./auth-button.component.css']
+})
+export class AuthButtonComponent {
+}
