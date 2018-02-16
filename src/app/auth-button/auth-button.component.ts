c60d24439d8daf517b9c6eed86624e3f831be3ab
nobody <nobody@gmail.com>
2018-02-17 01:41:45 +0300

    .

--- src/app/auth-button/auth-button.component.ts
+++ src/app/auth-button/auth-button.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-auth-button',
@@ -6,4 +7,11 @@ import {Component} from '@angular/core';
   styleUrls: ['./auth-button.component.css']
 })
 export class AuthButtonComponent {
+
+  constructor(private router: Router) {
+  }
+
+  openLogonPage() {
+    this.router.navigate(['/logon']);
+  }
 }
