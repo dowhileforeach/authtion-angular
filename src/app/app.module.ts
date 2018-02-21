25840c68e86c7642c677dcddd22014f8106a6692
z <alex@dwfe.ru>
2018-02-21 13:09:38 +0300

    redesign

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {AuthButtonComponent} from './auth-button/auth-button.component';
-import {LogonPageComponent} from './logon-page/logon-page.component';
+import {AuthButtonComponent} from './body-header/auth-button/auth-button.component';
+import {LogonPageComponent} from './page-logon/logon-page.component';
 import {FormsModule} from '@angular/forms';
-import {HomePageComponent} from './main-page/main-page.component';
+import {HomePageComponent} from './body-main/main-page/main-page.component';
 import {AppRoutingModule} from './app-routing.module';
 import { BodyFooterComponent } from './body-footer/body-footer.component';
 import { BodyMainComponent } from './body-main/body-main.component';
