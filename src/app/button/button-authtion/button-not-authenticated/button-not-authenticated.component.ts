435fa48ed296311330202ad247e7410e02b6a3f6
z <alex@dwfe.ru>
2018-04-02 08:53:51 +0300

    refact

--- src/app/button/button-authtion/button-not-authenticated/button-not-authenticated.component.ts
+++ src/app/button/button-authtion/button-not-authenticated/button-not-authenticated.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {MatDialog} from '@angular/material';
 import {PageLoginComponent} from '../../../page/login/page-login.component';
+import {Overlay} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-button-not-authenticated',
@@ -9,13 +10,14 @@ import {PageLoginComponent} from '../../../page/login/page-login.component';
 })
 export class ButtonNotAuthenticatedComponent {
 
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog, private overlay: Overlay) {
   }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(PageLoginComponent, {
       autoFocus: true, // focused on field with attribute 'cdkFocusInitial'
-      panelClass: 'login-register-dialog-panel-overriding'
+      panelClass: 'cdk-overlay-pane--login-register-dialog-overriding',
+      position: {top: '50px'}
     });
   }
 }
