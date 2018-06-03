b5314e2c25aaaa0e96fac9f6bd3a3ab4988efb6c
z <alex@dwfe.ru>
2018-06-03 08:03:55 +0300

    matInput on password

--- src/dwfe/components/form-controls/input-password/input-password.component.ts
+++ src/dwfe/components/form-controls/input-password/input-password.component.ts
@@ -1,7 +1,5 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-
-import {UtilsDwfe} from '@dwfe/classes/UtilsDwfe';
 
 @Component({
   selector: 'app-input-password-dwfe',
@@ -12,19 +10,15 @@ export class InputPasswordDwfeComponent implements OnInit {
   private minLength = 6;
   private maxLength = 55;
 
+  private group: FormGroup;
+  @Output() private takePasswordGroup = new EventEmitter<FormGroup>();
   private passwordControl: FormControl;
-  private passwordControlID = UtilsDwfe.randomStr(5, 'dwfe-form-group__password-'); // used in html template for a11y
-  @ViewChild('refPassword') private refPassword: ElementRef;
 
   @Input() private labelText = 'Password';
 
-  private group: FormGroup;
-  @Output() private takePasswordGroup = new EventEmitter<FormGroup>();
-
-  private formControlHasError = UtilsDwfe.formControlHasError;
-  private errorMessage = '';
-
   @Input() private tabIndexValue = 0;
+
+  private hide = true;
 
   ngOnInit() {
 
@@ -39,25 +33,5 @@ export class InputPasswordDwfeComponent implements OnInit {
     });
 
     this.takePasswordGroup.emit(this.group);
-  }
-
-  private get hasError(): boolean {
-    if (this.formControlHasError(this.passwordControl, 'required')) {
-      this.errorMessage = 'Required';
-      return true;
-    } else if (this.formControlHasError(this.passwordControl, 'minlength')) {
-      this.errorMessage = `Length must be >= ${this.minLength}`;
-      return true;
-    } else if (this.formControlHasError(this.passwordControl, 'maxlength')) {
-      this.errorMessage = `Length must be <= ${this.maxLength}`;
-      return true;
-    }
-    this.errorMessage = '';
-    return false;
-  }
-
-  private clearPassword() {
-    this.passwordControl.setValue('');
-    this.refPassword.nativeElement.focus();
   }
 }
